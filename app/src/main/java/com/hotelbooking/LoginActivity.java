e5b4b51e3695d254a6f5a48deaaac3f0f68554c3
guneetsapra <guneetsapra345@gmail.com>
2021-06-22 23:58:41 -0400

    Login Activity xml

---
+++ app/src/main/java/com/hotelbooking/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.hotelbooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+        
