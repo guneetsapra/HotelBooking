29344a16525f45ee368a1670da0526f9cb8ec660
shivanirana97 <shivanirana4049@gmail.com>
2021-06-23 00:21:35 -0400

    changes made in the login activity

--- app/src/main/java/com/hotelbooking/LoginActivity.java
+++ app/src/main/java/com/hotelbooking/LoginActivity.java
@@ -3,9 +3,15 @@ package com.hotelbooking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
 
 
         
