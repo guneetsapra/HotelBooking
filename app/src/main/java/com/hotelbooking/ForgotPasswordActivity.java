891468d04a5a57c8582f32051ac7c855008fd923
kumarluck377 <lakeshkumar0297@gmail.com>
2021-06-24 10:46:48 -0400

    Forgot Password xml added

---
+++ app/src/main/java/com/hotelbooking/ForgotPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.hotelbooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+}
