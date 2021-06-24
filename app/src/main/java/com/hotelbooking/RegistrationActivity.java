afa66339030daf6dd9967dbcd928dc13e51bdd5a
kumarluck377 <lakeshkumar0297@gmail.com>
2021-06-24 11:30:02 -0400

    Registration Activity.java changes

--- app/src/main/java/com/hotelbooking/RegistrationActivity.java
+++ app/src/main/java/com/hotelbooking/RegistrationActivity.java
@@ -35,6 +35,41 @@ public class RegistrationActivity extends AppCompatActivity {
         et_password = findViewById(R.id.et_password);
 
         btn_signup = findViewById(R.id.btn_signup);
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(et_name.getText().toString().isEmpty()){
+                    Toast.makeText(RegistrationActivity.this, "Please Enter Name..", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(et_username.getText().toString().isEmpty()){
+                    Toast.makeText(RegistrationActivity.this, "Please Enter email..", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(et_phone.getText().toString().isEmpty()){
+                    Toast.makeText(RegistrationActivity.this, "Please Enter Phone..", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(et_password.getText().toString().isEmpty()){
+                    Toast.makeText(RegistrationActivity.this, "Please Enter Password..", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                userRegistration();
+
+
+            }
+        });
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
 
 
     }
