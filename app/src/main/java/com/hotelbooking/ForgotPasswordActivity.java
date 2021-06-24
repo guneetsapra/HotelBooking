41004e1a74c1c6f565d54e2fcb2b22a8f5af286c
kumarluck377 <lakeshkumar0297@gmail.com>
2021-06-24 10:51:32 -0400

    ForgotPasswordActivity.java added

--- app/src/main/java/com/hotelbooking/ForgotPasswordActivity.java
+++ app/src/main/java/com/hotelbooking/ForgotPasswordActivity.java
@@ -7,9 +7,15 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
-
+    EditText et_email;
+    Button btn_adminlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        et_email = findViewById(R.id.et_email);
+        btn_adminlogin = findViewById(R.id.btn_adminlogin);
 
     }
 }
