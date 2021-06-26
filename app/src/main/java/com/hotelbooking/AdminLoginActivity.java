50420c6275410da3d53635b25089b0197563dd36
guneetsapra <guneetsapra345@gmail.com>
2021-06-26 03:41:15 -0400

    Updated AdminLoginActivity.java

--- app/src/main/java/com/hotelbooking/AdminLoginActivity.java
+++ app/src/main/java/com/hotelbooking/AdminLoginActivity.java
@@ -21,12 +21,35 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+package com.hotelbooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hotelbooking.api.ApiService;
+import com.hotelbooking.api.RetroClient;
+import com.hotelbooking.model.ResponseData;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class AdminLoginActivity extends AppCompatActivity {
 
     EditText et_adminusername,et_adminpassword;
     TextView tvuser;
     Button btn_adminlogin;
-    ProgressDialog pd;
+    ProgressDialog loading;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +70,8 @@ public class AdminLoginActivity extends AppCompatActivity {
                     Toast.makeText(AdminLoginActivity.this, "Please Enter Password..", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                //  startActivity(new Intent(AdminLoginActivity.this, AdminHomeActivity.class));
+
                 adminLogin();
             }
         });
@@ -61,5 +86,30 @@ public class AdminLoginActivity extends AppCompatActivity {
         });
     }
 
-
+    public  void adminLogin() {
+        //progreess bar loading
+        loading= new ProgressDialog(AdminLoginActivity.this);
+        loading.setTitle("Please wait,Data is being submit...");
+        loading.show();
+        // api call code
+        ApiService apiService = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<ResponseData> call = apiService.adminlogin(et_adminusername.getText().toString(),et_adminpassword.getText().toString());
+        call.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                loading.dismiss();
+                if (response.body().status.equals("true")) {
+                    startActivity(new Intent(AdminLoginActivity.this, AdminHomeActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(AdminLoginActivity.this, response.body().message, Toast.LENGTH_LONG).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                loading.dismiss();
+                Toast.makeText(AdminLoginActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
