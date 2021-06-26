6cb92ce7de927f27c6efce10b04458694e16a417
shivanirana97 <shivanirana4049@gmail.com>
2021-06-26 14:51:29 -0400

    Added Hotel Activity java file

---
+++ app/src/main/java/com/hotelbooking/AddHotelActivity.java
@@ -0,0 +1,108 @@
+package com.hotelbooking;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import com.hotelbooking.api.ApiService;
+import com.hotelbooking.model.ResponseData;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import pub.devrel.easypermissions.EasyPermissions;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class AddHotelActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
+    EditText etHotelName,etLocation,etAbout;
+    Button btn_upload,btnAddHotel;
+
+    ProgressDialog pd;
+    private static final String TAG = AddHotelActivity.class.getSimpleName();
+    private static final int REQUEST_GALLERY_CODE = 200;
+    private static final int READ_REQUEST_CODE = 300;
+    private static final String SERVER_PATH = "http://cegephotel.com/";
+    private Uri uri;
+    RatingBar rv_rating;
+    SharedPreferences sharedPreferences;
+    String session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_hotel);
+
+        getSupportActionBar().setTitle("Add Hotel");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+        session = sharedPreferences.getString("uname", "def-val");
+
+        etHotelName=(EditText)findViewById(R.id.etHotelName);
+        etLocation=(EditText)findViewById(R.id.etLocation);
+        etAbout=(EditText)findViewById(R.id.etAbout);
+        rv_rating=(RatingBar)findViewById(R.id.rv_rating);
+        btn_upload=(Button) findViewById(R.id.btn_upload);
+        btn_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK);
+                openGalleryIntent.setType("image/*");
+                startActivityForResult(openGalleryIntent, REQUEST_GALLERY_CODE);
+            }
+        });
+        btnAddHotel=(Button) findViewById(R.id.btnAddHotel);
+        btnAddHotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addHotelToServer();
+            }
+        });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, AddHotelActivity.this);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_GALLERY_CODE && resultCode == Activity.RESULT_OK){
+            uri = data.getData();
+            if(EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                String filePath = getRealPathFromURIPath(uri, AddHotelActivity.this);
+                file = new File(filePath);
+
+            }else{
+                EasyPermissions.requestPermissions(this, getString(R.string.read_file), READ_REQUEST_CODE, Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        }
+    }
+}
