7353f9122d8d3bf86681e4e2cd3451b3d2ef3690
shivanirana97 <shivanirana4049@gmail.com>
2021-06-26 15:12:13 -0400

    Added UserHomeActivity java file

---
+++ app/src/main/java/com/hotelbooking/UserHomeActivity.java
@@ -0,0 +1,62 @@
+package com.hotelbooking;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.hotelbooking.fragment.HomeFragment;
+import com.hotelbooking.fragment.ProfileFragment;
+
+public class UserHomeActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_home);
+
+        bottomNavigation();
+    }
+
+
+    private void bottomNavigation() {
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.action_item2);
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.action_item1:
+                                selectedFragment = HomeFragment.homeFragment();
+                                break;
+                            case R.id.action_item2:
+                                selectedFragment = HomeFragment.homeFragment();
+
+                                break;
+                            case R.id.action_item3:
+                                selectedFragment = ProfileFragment.profileFragment();
+
+                                break;
+
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_layout, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, HomeFragment.homeFragment());
+        transaction.commit();
+    }
+}
