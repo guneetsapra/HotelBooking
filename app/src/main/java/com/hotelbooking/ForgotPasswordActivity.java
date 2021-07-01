825775ee817313257452f143a2a22401bdac05e1
shivanirana97 <shivanirana4049@gmail.com>
2021-07-01 12:52:05 -0400

    Added Forgot Password Java File

--- app/src/main/java/com/hotelbooking/ForgotPasswordActivity.java
+++ app/src/main/java/com/hotelbooking/ForgotPasswordActivity.java
@@ -2,20 +2,58 @@ package com.hotelbooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.hotelbooking.api.ApiService;
+import com.hotelbooking.api.RetroClient;
+import com.hotelbooking.model.ResponseData;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+    String uname;
     EditText et_email;
-    Button btn_adminlogin;
+    Button btn_retrievepassword;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
         et_email = findViewById(R.id.et_email);
-        btn_adminlogin = findViewById(R.id.btn_adminlogin);
+        btn_retrievepassword = findViewById(R.id.btn_retrievepassword);
+        getSupportActionBar().setTitle("Forget Password");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+        uname=sharedPreferences.getString("user_name","");
+
+        btn_retrievepassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(et_email.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(),"Enter your Email",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                forgotPassword();
+            }
+        });
 
     }
-}
+    public  void forgotPassword()
+    {
+
+
+    }
+    }
