4c1e79c6e1e31fa005366e2fdefcc4b3dad3470f
shivanirana97 <shivanirana4049@gmail.com>
2021-07-01 13:11:52 -0400

    Added changes in Forgot Password Java File

--- app/src/main/java/com/hotelbooking/ForgotPasswordActivity.java
+++ app/src/main/java/com/hotelbooking/ForgotPasswordActivity.java
@@ -37,18 +37,55 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
-        uname = sharedPreferences.getString("user_name", "");
+        uname=sharedPreferences.getString("user_name","");
 
         btn_retrievepassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (et_email.getText().toString().isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Enter your Email", Toast.LENGTH_LONG).show();
+                if(et_email.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(),"Enter your Email",Toast.LENGTH_LONG).show();
                     return;
                 }
                 forgotPassword();
             }
         });
 
+    }
+    public  void forgotPassword()
+    {
+        String email=et_email.getText().toString();
+        ApiService apiService = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<ResponseData> call = apiService.getPassword(email);
+        call.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                if (response.body().message.equals("true")) {
+                    startActivity(new Intent(ForgotPasswordActivity.this, LoginActivity.class));
+                    finish();
+                    Toast.makeText(ForgotPasswordActivity.this, response.body().message, Toast.LENGTH_LONG).show();
+                   // Log.i("sample", "" + response.body().message);
+
+
+
+                } else {
+                    Toast.makeText(ForgotPasswordActivity.this, response.body().message, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                Log.d("sample","Response = "+t.toString());
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
