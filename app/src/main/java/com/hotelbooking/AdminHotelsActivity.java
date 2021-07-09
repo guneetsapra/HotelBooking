2822ff77ea55a33f2a8b0e977444bf9d88cb5dfc
shivanirana97 <shivanirana4049@gmail.com>
2021-07-09 12:28:02 -0400

    Admin Hotel Java Added

---
+++ app/src/main/java/com/hotelbooking/AdminHotelsActivity.java
@@ -0,0 +1,69 @@
+package com.hotelbooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.hotelbooking.adapters.AdminHotelListAdapter;
+import com.hotelbooking.api.ApiService;
+import com.hotelbooking.api.RetroClient;
+import com.hotelbooking.model.HotelPojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AdminHotelsActivity extends AppCompatActivity {
+
+    List<HotelPojo> hotelInfo;
+    ListView list_view;
+    ProgressDialog loading;
+    Button btnAddHotel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_hotels);
+        getSupportActionBar().setTitle("Hotels List");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btnAddHotel = (Button) findViewById(R.id.add_hotel);
+        btnAddHotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminHotelsActivity.this, AddHotelActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        list_view = (ListView) findViewById(R.id.hotel_list);
+
+        hotelInfo = new ArrayList<>();
+        getHotels();
+
+    }
+
+    public void getHotels() {
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
