9aa71757173fd6f48023ec1d579b4b03c8be9039
kumarluck377 <lakeshkumar0297@gmail.com>
2021-07-11 11:19:56 -0400

    Added Admin Hotel With Reviews java file

---
+++ app/src/main/java/com/hotelbooking/adapters/AdminHotelswithReviews.java
@@ -0,0 +1,115 @@
+package com.hotelbooking.adapters;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.hotelbooking.AdminHomeActivity;
+import com.hotelbooking.EditHotelActivity;
+import com.hotelbooking.R;
+import com.hotelbooking.ReviewsListActivity;
+import com.hotelbooking.api.ApiService;
+import com.hotelbooking.api.RetroClient;
+import com.hotelbooking.model.HotelPojo;
+import com.hotelbooking.model.ResponseData;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AdminHotelswithReviews extends BaseAdapter {
+    List<HotelPojo> hotelInfo;
+    Context con;
+    String url= "http://cegephotel.com/hotel/";
+
+    public AdminHotelswithReviews(List<HotelPojo> hotelInfo, Context con) {
+        this.hotelInfo = hotelInfo;
+        this.con = con;
+    }
+
+    @Override
+    public int getCount() {
+        return hotelInfo.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(final int pos, View view, ViewGroup viewGroup) {
+        LayoutInflater obj1 = (LayoutInflater) con.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View hotels = obj1.inflate(R.layout.adapter_adminviewrevieshotelslist, null);
+
+        ImageView hotelImage=(ImageView)hotels.findViewById(R.id.image);
+        Glide.with(con).load(url+hotelInfo.get(pos).getImage()).into(hotelImage);
+
+        Toast.makeText(con,hotelInfo.get(pos).getImage().toString(),Toast.LENGTH_LONG).show();
+
+        TextView tvHotelname = (TextView) hotels.findViewById(R.id.tvHotelname);
+        tvHotelname.setText(hotelInfo.get(pos).getHotel_name());
+
+        TextView tvLocation = (TextView) hotels.findViewById(R.id.tvLocation);
+        tvLocation.setText(hotelInfo.get(pos).getLocation());
+
+        RatingBar rv_rating= (RatingBar)hotels.findViewById(R.id.rv_rating);
+        rv_rating.setRating(Float.parseFloat(hotelInfo.get(pos).getRating()));
+
+        TextView tvReviews=(TextView)hotels.findViewById(R.id.tvReviews);
+        tvReviews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(con, ReviewsListActivity.class);
+                intent.putExtra("hid",hotelInfo.get(pos).getHid());
+                con.startActivity(intent);
+            }
+        });
+
+        return hotels;
+    }
+
+    ProgressDialog progressDialog;
+    public void deleteRoute(String id){
+        progressDialog = new ProgressDialog(con);
+        progressDialog.setMessage("Deleting Please wait....");
+        progressDialog.show();
+        ApiService service = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<ResponseData> call = service.deletehotel(id);
+        call.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                progressDialog.dismiss();
+                if(response.body()==null){
+                    Toast.makeText(con,"Server issue",Toast.LENGTH_SHORT).show();
+                }else {
+                    Intent intent=new Intent(con, AdminHomeActivity.class);
+                    con.startActivity(intent);
+                    Toast.makeText(con," Deleted successfully",Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(con, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
